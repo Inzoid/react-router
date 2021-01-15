39c9a45d998fbbcedb4056f3b49baab999f9cd80
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2021-01-16 00:15:49 +0700

    refactor code

--- src/component/Live.js
+++ src/component/Live.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col, Button, Card, Table, CardHeader, CardText, Input } from 'reactstrap';
+import { Row, Col, Button, Card, Table, CardHeader, CardText } from 'reactstrap';
 import axios from 'axios';
 
 import './team/Team.css';
@@ -29,8 +29,8 @@ function Live() {
       name: 'Anin'
     },
     {
-      id: '318217',
-      name: 'Lord Nunu'
+      id: '317745',
+      name: 'Lala'
     },
     {
       id: '318225',
@@ -48,18 +48,38 @@ function Live() {
       id: '318251',
       name: 'Ara'
     },
+    {
+      id: '317739',
+      name: 'Sisca'
+    },
+    {
+      id: '317738',
+      name: 'Feni'
+    },
+    {
+      id: '318219',
+      name: 'Dey'
+    },
   ]
 
-  const darkTable = {backgroundColor: '#333', borderColor: '#333', color: 'white'}
-  const lightTable = {backgroundColor: '#0000'}
+  const darkTable = { backgroundColor: '#333', borderColor: '#333', color: 'white' }
+  const lightTable = { backgroundColor: '#0000' }
 
   const [url, setUrl] = useState([]);
   const [menu, setMenu] = useState('showroom')
-  const [roomId, setRoomId] = useState(showroom[1].id);
+  const [roomId, setRoomId] = useState(showroom[0].id);
   const [comment, setComment] = useState([]);
   const [profile, setProfile] = useState();
   const [rank, setRank] = useState([]);
   const [theme, setTheme] = useState(lightTable)
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);
+  }, [menu, profile])
 
   useEffect(() => {
     axios.get(`/streaming_url?room_id=${roomId}`).then(res => {
@@ -83,23 +103,22 @@ function Live() {
     })
   }, [url, roomId])
 
-  const List = () => (
+  const Showroom = () => (
     <>
       <Row>
         <Col>
-          <Table>
-            <thead style={{backgroundColor: 'teal', color: 'white'}}>
+          <Table dark>
+            <thead style={{ backgroundColor: 'teal', color: 'white' }}>
               <tr>
                 <th colSpan="2">Daftar Showroom Member JKT48 </th>
               </tr>
             </thead>
             {showroom.map((item, idx) => (
-              <tbody style={theme}>
+              <tbody key={idx} style={theme}>
                 <tr>
                   <td>{item.name}</td>
                   <td>
                     <Button
-                      key={idx}
                       color="info"
                       onClick={() => setRoomId([item.id])}>
                       See Room
@@ -114,10 +133,6 @@ function Live() {
     </>
   )
 
-  const switchTheme = () => {
-    theme === darkTable ? setTheme(lightTable) : setTheme(darkTable)
-  }
-
   const Menu = () => (
     <Row>
       <Col>
@@ -125,14 +140,14 @@ function Live() {
           <>
             <Button
               color="info"
-              style={{ marginLeft: '6px' }}
+              className="showroom-menu"
               onClick={() => setMenu('chat')}
             >
               Live Chat
             </Button>
             <Button
               color="info"
-              style={{ marginLeft: '6px' }}
+              className="showroom-menu"
               onClick={() => setMenu('rank')}
             >
               Rank
@@ -141,42 +156,42 @@ function Live() {
         )}
         <Button
           color="danger"
-          style={{ marginLeft: '6px' }}
+          className="showroom-menu"
           onClick={() => setMenu('showroom')}
         >
           Showroom
         </Button>
         <Button
-          style={{ marginLeft: '6px' }}
+          className="showroom-menu"
           onClick={() => setTheme(darkTable)}
         >
-          Dark Theme
+          Dark
         </Button>
         <Button
           style={{ marginLeft: '6px' }}
           onClick={() => setTheme(lightTable)}
         >
-          Light Theme
+          Light
         </Button>
       </Col>
     </Row>
   )
 
-  const getDescription = () => {
-    let bio = profile.description
-    let description = bio.replace('\n', '<br />')
-    return bio
-  }
+  // const getDescription = () => {
+  //   let bio = profile.description
+  //   let description = bio.replace('\n', '<br />')
+  //   return bio
+  // }
 
   const UserProfile = () => (
     <Row style={{ marginLeft: '3px', marginRight: '3px' }}>
       {profile &&
         <>
           <Col sm="4">
-            <img width="300" src={profile.image} />
+            <img width="280" src={profile.image} />
             <CardHeader style={{ backgroundColor: '#dc3545', color: 'white', marginTop: '15px' }}>
               Biodata
-          </CardHeader>
+            </CardHeader>
             <Card style={{ borderTopLeftRadius: '0', borderTopRightRadius: '0' }} body outline color="danger">
               <CardText>
                 <b>Name:</b> {profile.room_name} <br />
@@ -188,28 +203,22 @@ function Live() {
           <Col sm="8">
             <h3 className="mt-3">Showroom is Offline</h3>
             {loading ? <Loading isLoad={loading} /> :
-              <>
-                {/* <h3>Showroom is Offline</h3>
-              <b>Name:</b> {profile.room_name} <br />
-              <b>Follower:</b> {profile.follower_num} <br />
-              <b>Room Level: </b>{profile.room_level} */}
-                <CardHeader style={{ backgroundColor: '#dc3545', color: 'white', marginTop: '15px' }}>
-                  Fans Letter
+              <CardHeader style={{ backgroundColor: '#dc3545', color: 'white', marginTop: '15px' }}>
+                Fans Letter
               </CardHeader>
-              </>
             }
             <Card style={{ borderTopLeftRadius: '0', borderTopRightRadius: '0' }} body outline color="danger">
               <CardText>
                 {profile.recommend_comment_list != null &&
                   profile.recommend_comment_list.map((item, idx) => (
-                    <>
-                      <h5 key={idx}>
+                    <div key={idx}>
+                      <h5>
                         <img width="30" style={{ marginRight: '5px' }} src={item.user.image} />
                         {item.user.name}
                       </h5>
                       <p>{item.comment} </p>
                       <hr />
-                    </>
+                    </div>
                   ))
                 }
               </CardText>
@@ -220,93 +229,81 @@ function Live() {
     </Row>
   )
 
-  //Set Loading
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 1000);
-  }, [menu, profile])
+  const UserRank = () => (
+    rank != null && rank.length ? (
+      rank.slice(0, 17).map((item, idx) => (
+      <>
+        <img key={idx} alt="rank" width="50" style={{ marginRight: '4px' }} src={item.user.avatar_url} />
+      </> 
+    ))) : url != null && (
+      <h4>Loading...</h4>
+    )
+  )
 
   return (
-    <>
-      <Row style={{ marginLeft: '3px', marginRight: '3px' }}>
-        <Col sm="8" className="mb-3">
-          {url ?
-            url.slice(0, 1).map((item, idx) => (
-              <>
-                <Stream key={idx} url={item.url} />
-              </>
-            ))
-            : profile &&
-              loading ? <Loading isLoad={loading} /> :
-              <UserProfile />
-          }
-          {
-            url != null && profile &&
-            <Col sm="8">
-              <h3>
-                {profile.room_url_key}
-                <Button
-                  style={{ marginLeft: '4px', paddingTop: '5px' }}
-                  color="primary"
-                >
-                  <img src="https://www.flaticon.com/svg/vstatic/svg/20/20079.svg?token=exp=1610707671~hmac=d9060d36240549cf1e511b8dab206899"
-                    width="18"
-                    className="mb-1 mr-1"
-                  /> {profile.view_num}
-                </Button>
-              </h3>
-            </Col>
-          }
-          {rank != null && rank.length ?
-            rank.slice(0, 17).map((item, idx) => (
-              <>
-                <img width="50" style={{ marginRight: '4px' }} src={item.user.avatar_url} />
-              </>
-            )) : url != null && (
-              <h4>Loading...</h4>
-            )
-          }
-        </Col>
-        <Col sm="4">
-          <Menu />
-          <Card body outline color="info" style={theme}>
-            {menu === 'chat' ? (
-              comment && comment.length ?
-                comment.slice(0, 100).map((item, idx) => (
-                  <>
-                    <h5 key={idx}>
-                      <img width="30" style={{ marginRight: '4px' }} src={item.avatar_url} />
-                      {item.name}
-                    </h5>
-                    <p>{item.comment}</p>
-                    <hr />
-                  </>
-                )) : (
-                  <Loading isLoad={loading} />
-                )
-            ) : menu == 'rank' ? (
-              <Table>
-                {rank.map((item, idx) => (
-                  <tbody>
-                    <tr>
-                      <th key={idx} scope="row">{item.order_no}</th>
-                      <td><img width="40" src={item.user.avatar_url} /></td>
-                      <td>{item.user.name}</td>
-                    </tr>
-                  </tbody>
-                ))}
-              </Table>
-            ) : menu == 'showroom' && (
-              <List />
-            )}
-          </Card>
-        </Col>
-      </Row>
-    </>
+    <Row style={{ marginLeft: '3px', marginRight: '3px' }}>
+      <Col sm="8">
+        {url ?
+          url.slice(0, 1).map((item, idx) => (
+            <Stream key={idx} url={item.url} />
+          )) : profile && (
+            loading ? <Loading isLoad={loading} /> :
+            <UserProfile />
+          )
+        }
+        {url != null && profile &&
+          <Col sm="8">
+            <h4 className="mt-1">
+              {profile.main_name.slice(0, -8)}
+              <Button
+                style={{ marginLeft: '4px', paddingTop: '5px' }}
+                color="primary"
+              >
+                <img src="https://pbs.twimg.com/media/Erx2IE1VQAEqdZD?format=png&name=small"
+                  width="15"
+                  className="mb-1 mr-1"
+                /> {profile.view_num}
+              </Button>
+            </h4>
+          </Col>
+        }
+        <UserRank />
+      </Col>
+      <Col sm="4">
+        <Menu />
+        <Card body outline color="info" style={theme}>
+          {menu === 'chat' ? (
+            comment && comment.length ?
+              comment.slice(0, 100).map((item, idx) => (
+                <>
+                  <h5 key={idx}>
+                    <img width="30" style={{ marginRight: '4px' }} src={item.avatar_url} />
+                    {item.name}
+                  </h5>
+                  <p>{item.comment}</p>
+                  <hr />
+                </>
+              )) : (
+                <Loading isLoad={loading} />
+              )
+          ) : menu == 'rank' ? (
+            <Table dark>
+              {rank.map((item, idx) => (
+                <tbody key={idx} >
+                  <tr>
+                    <th scope="row">{item.order_no}</th>
+                    <td><img width="40" src={item.user.avatar_url} /></td>
+                    <td>{item.user.name}</td>
+                  </tr>
+                </tbody>
+              ))}
+            </Table>
+          ) : menu == 'showroom' && (
+            <Showroom />
+          )}
+        </Card>
+      </Col>
+    </Row>
   );
 }
 
