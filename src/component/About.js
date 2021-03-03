c11a980856e9f57a5ac69f5f33b5404f584feab5
inzoid <inzoid21@gmail.com>
2021-03-03 10:31:15 +0700

    add source website for about page

--- src/component/About.js
+++ src/component/About.js
@@ -5,12 +5,51 @@ function About() {
   return (
     <Container>
       <Row>
-        <Col style={{marginTop: '25px'}}>
+        <Col className="mt-3">
           <Card body outline color="info">
-          <CardTitle><h3>About</h3></CardTitle>
-          <CardText>
-            This site is developed by <a href="https://twitter.com/inzoid" target="_blank"><b>Inzoid</b></a> this website is work on progress <br /> Site is developed at 21 December 2020 
-          </CardText>
+            <CardTitle tag="h2">About</CardTitle>
+            <CardText>
+              This site is developed by{' '}
+              <a
+                href="https://twitter.com/inzoid"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <b>Inzoid</b>
+              </a>{' '}
+              this website is work on progress. <br /> 
+              Site is developed at 21 December 2020 <br />
+            </CardText>
+            <CardTitle tag="h4">Source:</CardTitle>
+            <CardText>
+              There are three websites that I have used as a source content for
+              info setlist, schedule, link, member and image asset
+            </CardText>
+            <ul>
+              <li>
+                <a
+                  href="https://jkt48.com/calendar/list?lang=id"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  jkt48.com
+                </a>
+              </li>
+              <li>
+                <a href="https://tiket.com" target="_blank" rel="noreferrer">
+                  tiket.com
+                </a>
+              </li>
+              <li>
+                <a
+                  href="http://stage48.net/wiki/index.php/JKT48"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  stage48.net
+                </a>
+              </li>
+            </ul>
           </Card>
         </Col>
       </Row>
